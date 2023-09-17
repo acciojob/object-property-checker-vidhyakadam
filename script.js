0c114b5b8563fe4be4e0dbd6dc7c34ce0bf34b17
Vidhya Parmeshwar Kadam <131935689+vidhyakadam@users.noreply.github.com>
2023-09-17 21:15:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	 return sampleObject.hasOwnProperty(key);
 }
 
 // Do not change the code below
